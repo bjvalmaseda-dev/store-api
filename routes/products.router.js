53ad48a3f387497da543faf0dfb5d6b398a96d94
Bárbaro Javier Valmaseda <bjvalmaseda.g@gmail.com>
2022-09-17 09:35:12 -0400

    :recycle: [ADD] correct status codes

--- routes/products.router.js
+++ routes/products.router.js
@@ -24,12 +24,17 @@ router.get("/filter", (req, res) => {
 
 router.get("/:id", (req, res) => {
   const { id } = req.params;
-  res.json({ id, name: "Producto 2", price: 50.0 });
+
+  if (id === "999") {
+    res.status(404).json({ message: "Not Found" });
+  } else {
+    res.status(200).json({ id, name: "Producto 2", price: 50.0 });
+  }
 });
 
 router.post("/", (req, res) => {
   const body = req.body;
-  res.json({
+  res.status(201).json({
     message: "created",
     data: body,
   });
